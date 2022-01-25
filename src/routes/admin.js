64ff49ae921f618766bb5c05224a8a54150cef6f
LaloT98 <78931784+LaloT98@users.noreply.github.com>
2022-01-25 08:03:06 -0600

    corregi error de registro

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,7 +6,7 @@ const Articulos = require('../models/Article');
 const { format } = require('timeago.js');
 
 
-router.get("/menu", isAuthenticated, async (req, res) => {
+router.get("/menu", isAuthenticated, async(req, res) => {
     const usuario = await User.findById(req.user.id).lean()
     const articulos = await Articulos.find({ user: req.user.id }).sort({ date: 'desc' }).lean();
     const fecha = format(usuario.date)
@@ -18,12 +18,12 @@ router.get("/menu", isAuthenticated, async (req, res) => {
     }
 });
 
-router.get('/informacion', isAuthenticated, async (req, res) => {
+router.get('/informacion', isAuthenticated, async(req, res) => {
     const usuario = await User.findById(req.user.id).lean()
-    res.render('articulos/informacionUsuario',{usuario});
+    res.render('articulos/informacionUsuario', { usuario });
 });
 
-router.get('/menu/reporte/administradores', isAuthenticated, async (req, res) => {
+router.get('/menu/reporte/administradores', isAuthenticated, async(req, res) => {
     const usuario = await User.findById(req.user.id).lean()
     const TodosAdministradores = await User.find({ tipo: "ADMINISTRADOR" }).sort({ date: 'desc' }).lean();
     if (usuario.tipo == "USUARIO") {
@@ -34,7 +34,7 @@ router.get('/menu/reporte/administradores', isAuthenticated, async (req, res) =>
     }
 });
 
-router.get('/menu/reporte/usuarios', isAuthenticated, async (req, res) => {
+router.get('/menu/reporte/usuarios', isAuthenticated, async(req, res) => {
     const usuario = await User.findById(req.user.id).lean()
     const TodosUsuarios = await User.find({ tipo: "USUARIO" }).sort({ date: 'desc' }).lean();
     if (usuario.tipo == "USUARIO") {
@@ -45,7 +45,7 @@ router.get('/menu/reporte/usuarios', isAuthenticated, async (req, res) => {
     }
 });
 
-router.get('/menu/reporte/articulos', isAuthenticated, async (req, res) => {
+router.get('/menu/reporte/articulos', isAuthenticated, async(req, res) => {
     const usuario = await User.findById(req.user.id).lean()
     const TodosArticulos = await Articulos.find().sort({ user: 'desc', tipo: 'desc', date: 'desc' }).lean();
     if (usuario.tipo == "USUARIO") {
@@ -56,7 +56,7 @@ router.get('/menu/reporte/articulos', isAuthenticated, async (req, res) => {
     }
 });
 
-router.get('/menu/reporte/todo', isAuthenticated, async (req, res) => {
+router.get('/menu/reporte/todo', isAuthenticated, async(req, res) => {
     const usuario = await User.findById(req.user.id).lean()
     const TodosArticulos = await Articulos.find().sort({ user: 'desc', tipo: 'desc', date: 'desc' }).lean();
     const TodosUsuarios = await User.find({ tipo: "USUARIO" }).sort({ date: 'desc' }).lean();
@@ -69,13 +69,13 @@ router.get('/menu/reporte/todo', isAuthenticated, async (req, res) => {
     }
 });
 
-router.get('/menu/registrar', isAuthenticated, async (req, res) => {
+router.get('/menu/registrar', isAuthenticated, async(req, res) => {
     const usuario = await User.findById(req.user.id).lean();
     if (usuario.tipo == "USUARIO") {
         res.redirect("/menu")
     }
     if (usuario.tipo == "ADMINISTRADOR") {
-        res.render('users/registrarUsuario');
+        res.render('users/RegistrarUsuario');
     }
 
 });
