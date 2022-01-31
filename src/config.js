a4ac2f89d26d9acb11ea3d1fb8f05eecd2d2447d
LaloT98 <78931784+LaloT98@users.noreply.github.com>
2022-01-31 09:48:31 -0600

    Delete config.js

--- src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    uriEmail: "https://developers.google.com/oauthplayground",
-    userEmail: "eduardotzitzihuagarcia65@gmail.com",
-    clientId: "577889025465-lmht535r3ru2o9ip4prahq4tf4p5liak.apps.googleusercontent.com",
-    clientSecret: "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",
-    refreshToken: "1//04IPx0yCCPfOvCgYIARAAGAQSNwF-L9IrN95cWNMsucX002EpZLUK146bq0t7I6gg391JCtgNo1AfKbOhyfnDbpJAStZ17L25Tqc",
-    mongoID: "mongodb+srv://linux:LALO14629@cluster0.pdafw.mongodb.net/DB_articulos?retryWrites=true&w=majority"
-}
